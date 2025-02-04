aa527d9331ba8637ee1bf34a6fa6ac5d3f5a978f
victory balogun <victorybalogun@gmail.com>
2025-02-04 14:02:49 +0100

    created swagger.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const connectDB = require('./config/db');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerSetup = require('./swagger/swagger');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -16,75 +15,8 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-// Swagger Configuration
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Marketing Application API',
-      version: '1.0.0',
-      description: 'API for managing marketing activities, dashboards, forms, stock requests, roles, etc.',
-    },
-    tags: [
-      {
-        name: 'Auth',
-        description: 'User authentication and registration',
-      },
-      {
-        name: 'Activities',
-        description: 'Manage marketing activities',
-      },
-      {
-        name: 'Forms',
-        description: 'Manage forms for marketing activities',
-      },
-      {
-        name: 'Agents',
-        description: 'Agent authentication and form management',
-      },
-      {
-        name: 'Stock Requests',
-        description: 'Manage stock requests',
-      },
-      {
-        name: 'Dashboard',
-        description: 'Fetch dashboard metrics and insights',
-      },
-      {
-        name: 'Settings',
-        description: 'Manage roles and system settings',
-      },
-      {
-        name: 'Roles',
-        description: 'Manage user roles and permissions',
-      }
-    ],
-    servers: [
-      {
-        url: `http://localhost:${PORT}`,
-        description: 'Local Development Server',
-      },
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [],
-      },
-    ],
-  },
-  apis: ['./routes/*.js'],
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+// Swagger
+swaggerSetup(app);
 
 // Database Connection
 connectDB();
---
+++ swagger/swagger.js
@@ -0,0 +1,76 @@
+// Swagger Configuration
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const PORT = process.env.PORT || 5000;
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Marketing Application API',
+        version: '1.0.0',
+        description: 'API for managing marketing activities, dashboards, forms, stock requests, roles, etc.',
+      },
+      tags: [
+        {
+          name: 'Auth',
+          description: 'User authentication and registration',
+        },
+        {
+          name: 'Activities',
+          description: 'Manage marketing activities',
+        },
+        {
+          name: 'Forms',
+          description: 'Manage forms for marketing activities',
+        },
+        {
+          name: 'Agents',
+          description: 'Agent authentication and form management',
+        },
+        {
+          name: 'Stock Requests',
+          description: 'Manage stock requests',
+        },
+        {
+          name: 'Dashboard',
+          description: 'Fetch dashboard metrics and insights',
+        },
+        {
+          name: 'Settings',
+          description: 'Manage roles and system settings',
+        },
+        {
+          name: 'Roles',
+          description: 'Manage user roles and permissions',
+        }
+      ],
+      servers: [
+        {
+          url: `http://localhost:${PORT}`,
+          description: 'Local Development Server',
+        },
+      ],
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+      },
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+    },
+    apis: ['./routes/*.js'],
+  };
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+module.exports = (app) => {
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+};
